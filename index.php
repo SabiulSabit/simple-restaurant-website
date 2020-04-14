698fb2c4f9061ddbae560a98dfeda9750f8e85cb
sabiulsabit <iamsabit99@gmail.com>
2020-04-14 18:48:59 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+
+    <title>Document</title>
+    <link href="/assets/styles.css" rel="stylesheet">
+</head>
+
+<body id="final-example">
+
+    <div class="wrapper">
+        <div id="banner">
+            <a href="" title="Return to Home">
+                <img src="/img/banner.png" alt="Banner Logo">
+            </a>
+        </div>
+        <div id="nav">
+             <ul>
+                 <li><a href="index.php">Home</a> </li>
+                 <li><a href="menu.php">Menu</a> </li>
+                 <li><a href="team.php">Team</a> </li>
+                 <li><a href="contact.php">Contact</a> </li>
+             </ul>
+        </div>
+        <div class="content">
+            <div id="footer" class="cf">
+                <div class="column three">
+                    <strong>Phone</strong>
+                    +880 1724295204
+                </div>
+                <div class="column three">
+                    <strong>Location</strong>
+                    Dhanmondi<br>
+                    Dhaka,Bangladesh
+                </div>
+                <div class="column three last">
+                    <strong>Houres</strong>
+                    <em>Tuesday - Thursday </em><br>
+                    9.00AM - 10.00PM<br><br>
+
+                    <em>Friady - Satureday </em><br>
+                    4.00PM - 10.00PM<br><br>
+                </div>
+            </div>
+            <small>&copy;2020 SabitTheBot</small>
+        </div>
+    </div>
+    <div class="copyright-info">
+        <?php ?>
+    </div>
+</body>
+
+</html>
