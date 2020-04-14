0460ffe6513cca7db4dcca9ce22aeb2694bf709c
sabiulsabit <iamsabit99@gmail.com>
2020-04-14 19:20:18 +0600

    add includes folder and separate nav,footer and header

---
+++ includes/arrays.php
@@ -0,0 +1,25 @@
+
+<?php
+
+//nav iteams
+$navItems = array(
+
+    array(
+        "slug" => "index.php",
+        "title" => "Home"
+    ), 
+    array(
+        "slug" => "menu.php",
+        "title" => "Menu"
+    ),
+    array(
+        "slug" => "team.php",
+        "title" => "Team"
+    ),
+    array(
+        "slug" => "contact.php",
+        "title" => "Contact"
+    ),
+
+);
+?>
---
+++ includes/footer.php
@@ -0,0 +1,30 @@
+
+
+            <div id="footer" class="cf">
+                <div class="column three">
+                    <strong>Phone</strong>
+                    +880 1724295204
+                </div>
+                <div class="column three">
+                    <strong>Location</strong>
+                    Dhanmondi<br>
+                    Dhaka,Bangladesh
+                </div>
+                <div class="column three last">
+                    <strong>Houres</strong>
+                    <em>Tuesday - Thursday </em><br>
+                    9.00AM - 10.00PM<br><br>
+
+                    <em>Friady - Satureday </em><br>
+                    4.00PM - 10.00PM<br><br>
+                </div>
+            </div>
+            <small>&copy;2020 SabitTheBot</small>
+        </div>
+    </div>
+    <div class="copyright-info">
+       
+    </div>
+</body>
+
+</html>
