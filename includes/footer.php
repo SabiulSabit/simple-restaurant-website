bca1d8718be5cef259b2fa069fa3e68f859560c0
sabiulsabit <iamsabit99@gmail.com>
2020-04-14 20:06:48 +0600

    update footer and add shop close  open msg dynamicaly

--- includes/footer.php
+++ includes/footer.php
@@ -1,30 +1,30 @@
-
-
-            <div id="footer" class="cf">
-                <div class="column three">
-                    <strong>Phone</strong>
-                    +880 1724295204
-                </div>
-                <div class="column three">
-                    <strong>Location</strong>
-                    Dhanmondi<br>
-                    Dhaka,Bangladesh
-                </div>
-                <div class="column three last">
-                    <strong>Houres</strong>
-                    <em>Tuesday - Thursday </em><br>
-                    9.00AM - 10.00PM<br><br>
-
-                    <em>Friady - Satureday </em><br>
-                    4.00PM - 10.00PM<br><br>
-                </div>
-            </div>
-            <small>&copy;2020 SabitTheBot</small>
-        </div>
+<div id="footer" class="cf">
+    <div class="column three">
+        <strong>Phone</strong>
+        +880 1724295204
     </div>
-    <div class="copyright-info">
-       
+    <div class="column three">
+        <strong>Location</strong>
+        Dhanmondi<br>
+        Dhaka,Bangladesh
     </div>
+    <div class="column three last">
+        <strong>Houres</strong>
+        <em>Tuesday - Thursday </em><br>
+        9.00AM - 10.00PM<br><br>
+
+        <em>Friady - Satureday </em><br>
+        4.00PM - 10.00PM<br><br>
+
+        <?php include('includes/store-hours.php'); ?>
+    </div>
+</div>
+<small>&copy;<?php echo date('Y');  echo   "  ",$ownerName ?> </small>
+</div>
+</div>
+<div class="copyright-info">
+
+</div>
 </body>
 
 </html>
--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,5 @@
 <?php 
+  $ownerName = "SabitTheBot";
   include('includes/arrays.php');
 ?>
 
